f9248c7bddec760063fcdf4a7138f84da443ac25
六老师 <n/a>
2024-06-21 20:51:42 +0800

    ut

--- src/junit/UnitTest.java
+++ src/junit/UnitTest.java
@@ -15,20 +15,28 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class UnitTest {
     @Test
-    public void testMock()  {
+    public void throwsNPE()  {
+        List<Integer> l = mock(ArrayList.class);
+        doCallRealMethod().when(l).add(any(Integer.class));
+        // java.lang.NullPointerException: Cannot read the array length because "elementData" is null
+        l.add(100);
+        l.size();
+        verify(l, times(1)).add(0, 100);
+        verify(l, times(1)).size();
+        Integer result = l.get(0);
+        assertEquals(100, result);
+    }
 
-        List<Integer> myList = mock(ArrayList.class);
-
-        doCallRealMethod().when(myList).add(any(Integer.class));
-
-        myList.add(15);
-        myList.size();
-
-        verify(myList, times(1)).add(0, 15);
-        verify(myList, times(1)).size();
-
-        Integer result = myList.get(0);
-        assertEquals(15, result);
+    @Test
+    public void doNotthrowsNPE()  {
+        List<Integer> l = spy(ArrayList.class);
+        doCallRealMethod().when(l).add(any(Integer.class));
+        l.add(100);
+        l.size();
+        verify(l, times(1)).add(100);
+        verify(l, times(1)).size();
+        Integer result = l.get(0);
+        assertEquals(100, result);
     }
 
 
